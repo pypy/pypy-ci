5a86091231eb3cc07cc2738d909d2ce167ac7482
mattip <matti.picus@gmail.com>
2026-06-14 09:27:49 +0300

    update openssl to 3.0.21

--- docker/install_openssl.sh
+++ docker/install_openssl.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -xeo pipefail
 
-OPENSSL_URL="https://github.com/openssl/openssl/releases/download/OpenSSL_1_1_1s/"
-OPENSSL_NAME="openssl-1.1.1s"
-OPENSSL_SHA256="c5ac01e760ee6ff0dab61d6b2bbd30146724d063eb322180c6f18a6f74e4b6aa"
+OPENSSL_URL="https://github.com/openssl/openssl/releases/download/openssl-3.0.21"
+OPENSSL_NAME="openssl-3.0.21"
+OPENSSL_SHA256="617e29af8e421f46649484a4937e48c685e47f46488167c982f88bc4ec1d522f"
 
 function check_sha256sum {
     local fname=$1
@@ -13,22 +13,18 @@ function check_sha256sum {
     rm "${fname}.sha256"
 }
 
-echo args are $1 $2
-
 curl -sSL -#O "${OPENSSL_URL}/${OPENSSL_NAME}.tar.gz"
 check_sha256sum ${OPENSSL_NAME}.tar.gz ${OPENSSL_SHA256}
 tar zxf ${OPENSSL_NAME}.tar.gz
 PATH=/opt/perl/bin:$PATH
 pushd ${OPENSSL_NAME}
-if [ "$2" == "m32" ]; then
+if [ "$1" == "m32" ]; then
   setarch i386 ./Configure no-comp no-shared no-dynamic-engine -m32 linux-generic32 --prefix=/usr/local --openssldir=/usr/local
 else
   ./config no-comp enable-ec_nistp_64_gcc_128 no-shared no-dynamic-engine --prefix=/usr/local --openssldir=/usr/local
 fi
 make depend
 make -j4
-# avoid installing the docs
-# https://github.com/openssl/openssl/issues/6685#issuecomment-403838728
 make install_sw install_ssldirs
 popd
 rm -rf openssl*
